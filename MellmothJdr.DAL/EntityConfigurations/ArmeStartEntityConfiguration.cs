9927eb24ecb0a250889957c97827b9feb3dd9c3d
vidalj13 <44831477+vidalj13@users.noreply.github.com>
2025-03-12 10:21:45 +0100

    wip

--- MellmothJdr.DAL/EntityConfigurations/ArmeStartEntityConfiguration.cs
+++ MellmothJdr.DAL/EntityConfigurations/ArmeStartEntityConfiguration.cs
@@ -23,20 +23,39 @@ namespace NotificationManager.Infrastructure.EntityConfigurations
                 .HasForeignKey(x => x.ClasseId);
 
             DateTime dateInit = new(2025, 02, 25);
-            /*
+            
             builder.HasData(new List<SoinStart>() {
                     new()
-                    {
-                        Libelle = "Pétoire",
-                        Id = new Guid("626B4CBC-24FD-4CF4-B0B8-6CE09644A9E1"),
-                        ClasseId = Ids.ChroniqueOublie.Classe.,
+                    {                    
+                        Libelle = "Potion de soins",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Ensorceleur,
                         DateCrea = dateInit,
                         DateMaj = dateInit,
-                        DeSoin = 10,
-                        NumbreDeSoin = 1
+                        DeSoin = 8,
+                        NumbreDeSoin = 1,
+                    },
+                    new()
+                    {                    
+                        Libelle = "Potion de soins",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Magicien,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        DeSoin = 8,
+                        NumbreDeSoin = 1,
+                    },
+                    new()
+                    {                    
+                        Libelle = "Potion de soins",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Necromancien,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        DeSoin = 8,
+                        NumbreDeSoin = 1,
                     },
             });
-            */
         }
     }
     internal class ArmeStartEntityConfiguration : IEntityTypeConfiguration<ArmeStart>
@@ -69,7 +88,7 @@ namespace NotificationManager.Infrastructure.EntityConfigurations
                     },
                     new()
                     {
-                        Libelle = "Epée longue",
+                        Libelle = "Épée longue",
                         Id = new Guid("7B82ED7E-BA93-4373-939E-28AC1721C088"),
                         ClasseId = Ids.ChroniqueOublie.Classe.Arquebusier,
                         DateCrea = dateInit,
@@ -122,7 +141,7 @@ namespace NotificationManager.Infrastructure.EntityConfigurations
                     },
                     new()
                     {
-                        Libelle = "Rapiere",
+                        Libelle = "Rapière",
                         Id = new Guid("DEEDD7E0-F790-4ADA-B987-D95B9C09D45B"),
                         ClasseId = Ids.ChroniqueOublie.Classe.Barde,
                         DateCrea = dateInit,
@@ -149,7 +168,305 @@ namespace NotificationManager.Infrastructure.EntityConfigurations
                         DateCrea = dateInit,
                         DateMaj = dateInit
                     },
-                });
+                    new()
+                    {
+                        Libelle = "Épée longue",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Chevalier,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        DeAttaque = 8,
+                        NumbreDeAttaque = 1,
+                        PorteEnM = null
+                    },
+                    new()
+                    {
+                        Libelle = "Grand bouclier",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Chevalier,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        ModificateurDef = 2,
+                    },
+                    new()
+                    {
+                        Libelle = "Lance de cavalerie",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Chevalier,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        DeAttaque = 6,
+                        NumbreDeAttaque = 2
+                    },
+                    new()
+                    {
+                        Libelle = "Dague",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Chevalier,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        DeAttaque = 4,
+                        NumbreDeAttaque = 1
+                    },
+                    new()
+                    {
+                        Libelle = "Bâton",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Druide,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        DeAttaque = 6,
+                        NumbreDeAttaque = 1,
+                    },
+                    new()
+                    {
+                        Libelle = "Dague",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Druide,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        DeAttaque = 4,
+                        NumbreDeAttaque = 1,
+                    },
+                    new()
+                    {
+                        Libelle = "Arc court",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Druide,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        DeAttaque = 6,
+                        NumbreDeAttaque = 1,
+                        PorteEnM = 30
+                    },
+                    new()
+                    {
+                        Libelle = "Bâton ferré",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Ensorceleur,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        DeAttaque = 6 ,
+                        NumbreDeAttaque = 1
+                    },
+                    new()
+                    {
+                        Libelle = "Dague",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Ensorceleur,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        DeAttaque = 4,
+                        NumbreDeAttaque = 1,
+                    },
+                    new()
+                    {
+                        Libelle = "Dague",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Forgesort,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        DeAttaque = 4,
+                        NumbreDeAttaque = 1,
+                    },
+                    new()
+                    {
+                        Libelle = "Bâton ferré",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Forgesort,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        DeAttaque = 6 ,
+                        NumbreDeAttaque = 1
+                    },
+                    new()
+                    {
+                        Libelle = "Marteau",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Forgesort,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        DeAttaque = 6,
+                        NumbreDeAttaque = 1
+                    },
+                    new()
+                    {
+                        Libelle = "Épée longue",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Guerrier,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        DeAttaque = 8,
+                        NumbreDeAttaque = 1
+                    },
+                    new()
+                    {
+                        Libelle = "Épée ou hache à 2 mains",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Guerrier,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        DeAttaque = 6,
+                        NumbreDeAttaque = 2                   
+                    },
+                    new()
+                    {
+                        Libelle = "Grand bouclier",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Guerrier,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        ModificateurDef = 2,
+                    },
+                    new()
+                    {
+                        Libelle = "Bâton",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Magicien,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        DeAttaque = 6,
+                        NumbreDeAttaque = 1,
+                    },
+                    new()
+                    {
+                        Libelle = "Grimoire",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Magicien,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                    },
+                    new()
+                    {
+                        Libelle = "Dague",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Magicien,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        DeAttaque = 4,
+                        NumbreDeAttaque = 1,
+                    },
+                    new()
+                    {
+                        Libelle = "Bâton ferré",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Moine,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        DeAttaque = 6 ,
+                        NumbreDeAttaque = 1
+                    },
+                    new()
+                    {
+                        Libelle = "Dague",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Nécromancien,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        DeAttaque = 4,
+                        NumbreDeAttaque = 1,
+                    },
+                    new()
+                    {
+                        Libelle = "Bâton ferré",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Nécromancien,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        DeAttaque = 6 ,
+                        NumbreDeAttaque = 1
+                    },
+                    new()
+                    {
+                        Libelle = "Petit bouclier",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Prêtre,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        ModificateurDef = 1,
+                    },
+                    new()
+                    {
+                        Libelle = "Marteau ou masse de guerre",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Prêtre,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        DeAttaque = 6,
+                        NumbreDeAttaque = 1
+                    },
+                    new()
+                    {
+                        Libelle = "Épée longue",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Rôdeur,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        DeAttaque = 8,
+                        NumbreDeAttaque = 1,
+                    },
+                    new()
+                    {
+                        Libelle = "Arc court",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Rôdeur,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        DeAttaque = 6,
+                        NumbreDeAttaque = 1,
+                        PorteEnM = 30
+                    },
+                    new()
+                    {
+                        Libelle = "Dague",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Rôdeur,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        DeAttaque = 4,
+                        NumbreDeAttaque = 1,
+                    },
+                    new()
+                    {
+                        Libelle = "Carquois",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Rôdeur,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                    },
+                    new()
+                    {
+                        Libelle = "Dague de jet",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Voleur,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        Quantite = 5
+                        DeAttaque = 4,
+                        NumbreDeAttaque = 1,
+                    },
+                    new()
+                    {
+                        Libelle = "Rapière",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Voleur,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        DeAttaque = 6,
+                        NumbreDeAttaque = 1,
+                        MinCritique = 19
+                    },
+                    new()
+                    {
+                        Libelle = "Outils de crochetage",
+                        Id = new Guid(""),
+                        ClasseId = Ids.ChroniqueOublie.Classe.Voleur,
+                        DateCrea = dateInit,
+                        DateMaj = dateInit,
+                        DeAttaque = ,
+                        NumbreDeAttaque = ,
+                        PorteEnM = 
+                    },                });
         }
     }
 }
